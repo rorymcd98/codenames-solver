87c03f3e20a219b055c62426d50e0126218161e0
rorymcd98 <rorymcd98@gmail.com>
2023-09-02 18:29:12 +0100

    initial frontend commit

---
+++ CardsState.cs
@@ -0,0 +1,49 @@
+﻿using codenames_solver.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace codenames_solver
+{
+    public class CardsState
+    {
+        private const int NUMBER_OF_CARDS = 25;
+
+        private List<CardInfo> cards;
+
+        public CardsState()
+        {
+            cards = new List<CardInfo>(NUMBER_OF_CARDS);
+            for (int i = 0; i < NUMBER_OF_CARDS; i++)
+            {
+                cards.Add(new CardInfo());
+            }
+        }
+
+        public CardColor GetCardColor(int CardIndex)
+        {
+            return cards[CardIndex].Color;
+        }
+
+        public void UpdateCardColor(ComponentBase Source, CardColor NewCardColor, int CardIndex)
+        {
+            this.cards[CardIndex].Color = NewCardColor;
+            NotifyStateChanged(Source, "CardColor");
+        }
+
+        public string GetCardText(int CardIndex)
+        {
+            return cards[CardIndex].Text;
+        }
+
+        public void UpdateCardText(ComponentBase Source, string NewCardText, int CardIndex)
+        {
+            this.cards[CardIndex].Text = NewCardText;
+            NotifyStateChanged(Source, "CardText");
+        }
+
+        public event Action<ComponentBase, string> StateChanged;
+
+        private void NotifyStateChanged(ComponentBase Source, string Property)
+            => StateChanged?.Invoke(Source, Property);
+    }
+}
---
+++ Shared/CardInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace codenames_solver.Shared
+{
+    public enum CardColor
+    {
+        Blue,
+        Red,
+        Black,
+        Neutral
+    }
+    public class CardInfo
+    {
+        public string Text { get; set; }
+        public CardColor Color { get; set; }
+        public CardInfo()
+        {
+            Text = string.Empty;
+            Color = CardColor.Neutral;
+        }
+    }
+}
